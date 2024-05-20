e886d81bac622df38c97e385351e37187633ca3a
Parksoorin <rin4330@naver.com>
2024-05-20 09:15:40 +0900

    lv1 기초 추가

---
+++ _01기초/src/연산자1_문제/연산자5_문제02_시간분초_문제.java
@@ -0,0 +1,16 @@
+package 연산자1_문제;
+
+/*
+	[문제]
+		321321초는 몇 시간 몇 분 몇 초인지 구하시오.
+	[정답]
+		89시간
+		15분
+		21초
+*/
+
+public class 연산자5_문제02_시간분초_문제 {
+	public static void main(String[] args) {
+		
+	}
+}
